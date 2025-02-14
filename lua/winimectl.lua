2a2e22f14a40e026aece086a0abe1214a6db971d
hirsaeki <5356955+hirsaeki@users.noreply.github.com>
2025-02-15 02:32:32 +0900

    fix #request arguments

--- lua/winimectl.lua
+++ lua/winimectl.lua
@@ -11,17 +11,12 @@ function M.setup(opts)
     group = group,
     callback = function()
       -- まず現在のIMEの状態を取得
-      vim.fn['denops#request'](
-        'winimectl',
-        'getImeStatus',
-        function(err, status)
-          if not err and status ~= nil then
-            -- 状態の保存に成功したら、IMEをオフにする
-            vim.b.prev_ime_status = status
-            vim.fn['denops#request']('winimectl', 'setImeStatus', { false })
-          end
-        end
-      )
+      local status = vim.fn['denops#request']('winimectl', 'getImeStatus', {})
+      if status ~= nil then
+        -- 状態の保存に成功したら、IMEをオフにする
+        vim.b.prev_ime_status = status
+        vim.fn['denops#request']('winimectl', 'setImeStatus', {false})
+      end
     end,
   })
 
@@ -32,7 +27,7 @@ function M.setup(opts)
       -- 保存されていた以前のIME状態を復元
       local prev_status = vim.b.prev_ime_status
       if prev_status ~= nil then
-        vim.fn['denops#request']('winimectl', 'setImeStatus', { prev_status })
+        vim.fn['denops#request']('winimectl', 'setImeStatus', {prev_status})
       end
     end,
   })
