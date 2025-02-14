22d71acbbc0965e17d16576b8be672ecdef71985
hirsaeki <5356955+hirsaeki@users.noreply.github.com>
2025-02-15 01:16:52 +0900

    fix and simplify error handling in lua.

--- lua/winimectl.lua
+++ lua/winimectl.lua
@@ -15,25 +15,12 @@ function M.setup(opts)
         'winimectl',
         'getImeStatus',
         {},
-        function(status)
-          if status ~= nil then
+        function(err, status)
+          if not err and status ~= nil then
             -- 状態の保存に成功したら、IMEをオフにする
             vim.b.prev_ime_status = status
-            vim.fn['denops#request'](
-              'winimectl',
-              'setImeStatus',
-              {false},
-              function(_)
-                -- 成功時は何もしない
-              end,
-              function(err)
-                vim.api.nvim_err_writeln("[winimectl] Failed to disable IME: " .. vim.inspect(err))
-              end
-            )
+            vim.fn['denops#request']('winimectl', 'setImeStatus', {false})
           end
-        end,
-        function(err)
-          vim.api.nvim_err_writeln("[winimectl] Failed to get IME status: " .. vim.inspect(err))
         end
       )
     end,
@@ -46,17 +33,7 @@ function M.setup(opts)
       -- 保存されていた以前のIME状態を復元
       local prev_status = vim.b.prev_ime_status
       if prev_status ~= nil then
-        vim.fn['denops#request'](
-          'winimectl',
-          'setImeStatus',
-          {prev_status},
-          function(_)
-            -- 成功時は何もしない
-          end,
-          function(err)
-            vim.api.nvim_err_writeln("[winimectl] Failed to restore IME status: " .. vim.inspect(err))
-          end
-        )
+        vim.fn['denops#request']('winimectl', 'setImeStatus', {prev_status})
       end
     end,
   })
